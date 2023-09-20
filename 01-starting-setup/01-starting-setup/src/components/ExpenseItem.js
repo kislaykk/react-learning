afac035d058fe68e078e0f855fcb2e554425e73b
Kislay Kanti Dhar <kislayof@gmail.com>
2023-09-20 22:26:51 +0530

    component is splitted

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
 import './ExpenseItem.css';
+import './ExpenseDate';
 
 function ExpenseItem(props) {
     
     return (
         <div className="expense-item">
-            <div>{props.date}</div>
+            <ExpenseItem
+                date ={props.date} 
+            />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
